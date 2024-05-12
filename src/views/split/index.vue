2399b692e94ad9c30c407f5469d5355bfbe66803
XingLing <979653431@qq.com>
2024-05-12 22:43:55 +0800

    splitpane

---
+++ src/views/split/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="page">
+    <SplitPane :pane-length-percent.sync="paneLengthPercent" />
+  </div>
+</template>
+
+<script>
+import SplitPane from "./split-pane";
+export default {
+  components: {
+    SplitPane,
+  },
+  data() {
+    return {
+      paneLengthPercent: 30,
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.page {
+  height: 60vh;
+  padding: 10px;
+  background: #000;
+}
+</style>

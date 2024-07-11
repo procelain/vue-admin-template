9c12de748b28fb60ae39f877064fffdf32fec354
XingLing <979653431@qq.com>
2024-07-11 18:01:10 +0800

    vxe-table

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import Vue from "vue";
-import Router from "vue-router";
+import Vue from 'vue'
+import Router from 'vue-router'
 
-Vue.use(Router);
+Vue.use(Router)
 
 /* Layout */
-import Layout from "@/layout";
+import Layout from '@/layout'
 
 /**
  * Note: sub-menu only appear when route children.length >= 1
@@ -32,57 +32,63 @@ import Layout from "@/layout";
  */
 export const constantRoutes = [
   {
-    path: "/login",
-    component: () => import("@/views/login/index"),
-    hidden: true,
+    path: '/login',
+    component: () => import('@/views/login/index'),
+    hidden: true
   },
 
   {
-    path: "/404",
-    component: () => import("@/views/404"),
-    hidden: true,
+    path: '/404',
+    component: () => import('@/views/404'),
+    hidden: true
   },
 
   {
-    path: "/",
+    path: '/',
     component: Layout,
-    redirect: "/dashboard",
+    redirect: '/dashboard',
     children: [
       {
-        path: "dashboard",
-        name: "Dashboard",
-        component: () => import("@/views/dashboard/index"),
-        meta: { title: "Dashboard", icon: "dashboard" },
-      },
-    ],
+        path: 'dashboard',
+        name: 'Dashboard',
+        component: () => import('@/views/dashboard/index'),
+        meta: { title: 'Dashboard', icon: 'dashboard' }
+      }
+    ]
   },
 
   {
-    path: "/example",
+    path: '/example',
     component: Layout,
-    redirect: "/example/table",
-    name: "Example",
-    meta: { title: "Example", icon: "el-icon-s-help" },
+    redirect: '/example/table',
+    name: 'Example',
+    meta: { title: 'Example', icon: 'el-icon-s-help' },
     children: [
       {
-        path: "split",
-        name: "Split",
-        component: () => import("@/views/split/index"),
-        meta: { title: "Split", icon: "tree" },
+        path: 'split',
+        name: 'Split',
+        component: () => import('@/views/split/index'),
+        meta: { title: 'Split', icon: 'tree' }
       },
       {
-        path: "dhtmlx-gantt",
-        name: "DhtmlxGantt",
-        component: () => import("@/views/DhtmlxGantt/index"),
-        meta: { title: "DhtmlxGantt", icon: "tree" },
+        path: 'dhtmlx-gantt',
+        name: 'DhtmlxGantt',
+        component: () => import('@/views/DhtmlxGantt/index'),
+        meta: { title: 'DhtmlxGantt', icon: 'tree' }
       },
       {
-        path: "x6",
-        name: "X6",
-        component: () => import("@/views/x6/index"),
-        meta: { title: "x6", icon: "tree" },
+        path: 'x6',
+        name: 'X6',
+        component: () => import('@/views/x6/index'),
+        meta: { title: 'x6', icon: 'tree' }
       },
-    ],
+      {
+        path: 'vxe-table',
+        name: 'vxe-table',
+        component: () => import('@/views/vxe-table/index'),
+        meta: { title: 'vxe-table', icon: 'tree' }
+      }
+    ]
   },
 
   // {
@@ -169,22 +175,22 @@ export const constantRoutes = [
   // },
 
   // 404 page must be placed at the end !!!
-  { path: "*", redirect: "/404", hidden: true },
-];
+  { path: '*', redirect: '/404', hidden: true }
+]
 
 const createRouter = () =>
   new Router({
     // mode: 'history', // require service support
     scrollBehavior: () => ({ y: 0 }),
-    routes: constantRoutes,
-  });
+    routes: constantRoutes
+  })
 
-const router = createRouter();
+const router = createRouter()
 
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
-  const newRouter = createRouter();
-  router.matcher = newRouter.matcher; // reset router
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher // reset router
 }
 
-export default router;
+export default router
---
+++ src/views/vxe-table/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <VxeTable />
+  </div>
+</template>
+
+<script>
+import VxeTable from "./vxe-table";
+export default {
+  components: {
+    VxeTable,
+  },
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss"></style>
